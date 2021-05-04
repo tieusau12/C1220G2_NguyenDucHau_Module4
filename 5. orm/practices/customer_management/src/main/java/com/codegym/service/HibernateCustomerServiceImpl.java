b2c5fe4bdf14ccd30136ceb0b61bd896849ee9dd
MoonHZ <58355523+tieusau12@users.noreply.github.com>
2021-05-05 00:03:55 +0700

    up orm vs jpa

---
+++ 5. orm/practices/customer_management/src/main/java/com/codegym/service/HibernateCustomerServiceImpl.java
@@ -0,0 +1,65 @@
+package com.codegym.service;
+
+import com.codegym.model.Customer;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
+
+@Service
+public class HibernateCustomerServiceImpl implements ICustomerService {
+    @Override
+    public List<Customer> findAll() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public Customer findOne(Long id) {
+        return null;
+    }
+
+    @Override
+    public Customer save(Customer customer) {
+        return null;
+    }
+
+    @Override
+    public List<Customer> save(List<Customer> customers) {
+        return null;
+    }
+
+    @Override
+    public boolean exists(Long id) {
+        return false;
+    }
+
+    @Override
+    public List<Customer> findAll(List<Long> ids) {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public long count() {
+        return 0;
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+
+    @Override
+    public void delete(Customer customer) {
+
+    }
+
+    @Override
+    public void delete(List<Customer> customers) {
+
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
+}
