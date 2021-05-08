45a64ec63677aa22c59e17073171becafdde4cd0
MoonHZ <58355523+tieusau12@users.noreply.github.com>
2021-05-09 00:00:34 +0700

    up

---
+++ 7.spring_data_repository/practices/src/main/java/com/codegym/demo/service/CustomerService.java
@@ -0,0 +1,37 @@
+package com.codegym.demo.service;
+
+import com.codegym.demo.model.Customer;
+import com.codegym.demo.repository.ICustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+public class CustomerService implements ICustomerService {
+
+    @Autowired
+    private ICustomerRepository customerRepository;
+
+    @Override
+    public List<Customer> findAll() {
+        return customerRepository.findAll();
+    }
+
+    @Override
+    public Customer findById(Long id) {
+        return customerRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public void save(Customer customer) {
+        customerRepository.save(customer);
+    }
+
+    @Override
+    public void remove(Long id) {
+        customerRepository.deleteById(id);
+    }
+
+}
---
+++ 7.spring_data_repository/practices/src/main/java/com/codegym/demo/service/IProvinceService.java
@@ -0,0 +1,6 @@
+package com.codegym.demo.service;
+
+import com.codegym.demo.model.Province;
+
+public interface IProvinceService extends IGeneralService<Province> {
+}
